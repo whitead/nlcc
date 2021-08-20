d9553e89f1d0966b5e6217b8027634bca3ee43a4
Andrew White <white.d.andrew@gmail.com>
2021-08-20 14:34:14 -0400

    Added multiline

--- nlcc/utils.py
+++ nlcc/utils.py
@@ -1,14 +1,29 @@
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.key_binding import KeyBindings
+
 
 def text_iter(cli_prompt):
+
+    kb = KeyBindings()
+
+    @kb.add('enter')
+    def _(event):
+        event.current_buffer.validate_and_handle()
+    @kb.add('escape', 'enter')
+    def _(event):
+        event.current_buffer.insert_text('\n')
+
+
     history = InMemoryHistory()
     session = PromptSession(
        history=history,
     auto_suggest=AutoSuggestFromHistory(),
     enable_history_search=True,
-    complete_while_typing=True
+    complete_while_typing=True,
+    multiline=True,
+    key_bindings=kb
     )
     while True:
         q = session.prompt(f'nlcc{cli_prompt[0]}:>')
